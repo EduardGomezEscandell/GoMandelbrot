22ee510b943d4deb846b7cea2834ea22ca3da417
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-06-26 14:32:54 +0200

    Movd everything to src

---
+++ src/image/imageIO/netpbm.go
@@ -0,0 +1,55 @@
+package imageIO
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/EduardGomezEscandell/GoMandelbrot/image"
+)
+
+const (
+	BINARY = iota
+	ASCII
+)
+
+func ppm_header(filehandle *os.File, img *image.Image, encoding int) {
+
+	if encoding == ASCII {
+		fmt.Fprintf(filehandle, "P3\n")
+	} else if encoding == BINARY {
+		fmt.Fprintf(filehandle, "P6\n")
+	} else {
+		panic("Unknown encoding")
+	}
+
+	fmt.Fprintf(filehandle, "%d %d\n", img.Width, img.Height)
+	fmt.Fprintf(filehandle, "255\n")
+	fmt.Fprintf(filehandle, "# %s\n", img.Title)
+}
+
+func ppm_body(filehandle *os.File, img *image.Image, encoding int) {
+	if encoding == ASCII {
+		for _, px := range img.Pixels {
+			fmt.Fprintf(filehandle, "%d %d %d ", px.R, px.G, px.B)
+		}
+	} else if encoding == BINARY {
+		for _, px := range img.Pixels {
+			filehandle.Write([]byte{px.R, px.G, px.B})
+		}
+	} else {
+		panic("Unknown encoding")
+	}
+}
+
+func ImagePPMOutput(img *image.Image, filename string, encoding int) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	ppm_header(file, img, encoding)
+	ppm_body(file, img, encoding)
+
+	return nil
+}
